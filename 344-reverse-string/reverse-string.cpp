c51591fd4811d488d6ed2cb84eca7dd80cf6376c
Mishthi Chaurasia <mishthichaurasia02@gmail.com>
2026-08-22 23:43:21 +0530

    Time: 0 ms (100.00%) | Memory: 28 MB (5.30%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-
-        int start = 0;
-        int end = s.size() - 1;
-
-        while(start <= end){
-
-            swap(s[start], s[end]);
-
-            start++;
-            end--;
+        //create a stack
+        stack<char> st;
+        //loop through the string, to add all the string elements in stack
+        for(int i=0;i<s.size();i++){
+            //create a variable to add the values
+            char ch = s[i];
+            st.push(ch);
+        }
+        //empty the original string
+        s.clear();
+        //add the stack elements to the string
+        while(!st.empty()){
+            char ch = st.top();
+            //reversed
+            s.push_back(ch);
+            st.pop();
         }
     }
 };
